f94e137ac6cce8da785f06793143d33abd50a90a
Aaron Jubbal <ajjubbal@gmail.com>
2014-03-01 20:03:22 -0800

    Added XCTests to sample project.

---
+++ SampleProject/SATextFieldTests/SATextFieldTests.m
@@ -0,0 +1,39 @@
+//
+//  SATextFieldTests.m
+//  SATextFieldTests
+//
+//  Created by Aaron Jubbal on 3/1/14.
+//  Copyright (c) 2014 Aaron Jubbal. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "SATextField.h"
+
+@interface SATextFieldTests : XCTestCase
+
+@property (nonatomic, strong) SATextField *textField;
+
+@end
+
+@implementation SATextFieldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    _textField = [[SATextField alloc] initWithFrame:CGRectZero];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testDelegates
+{
+    
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
